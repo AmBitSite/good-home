bf5762c5f941205e941f1d62465503254bad6703
AmBitSite <yfljvtymitgbnmrtif@yandex.ru>
2019-11-25 19:43:32 +0300

    25.11.2019

--- js/script.js
+++ js/script.js
@@ -197,7 +197,7 @@ function smoothScroll(eID) {
         scrollTo(0, stopY); return;
     }
     var speed = Math.round(distance / 100);
-    if (speed >= 20) speed = 20;
+    if (speed >= 40) speed = 40;
     var step = Math.round(distance / 25);
     var leapY = stopY > startY ? startY + step : startY - step;
     var timer = 0;
